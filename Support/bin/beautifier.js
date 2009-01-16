5ee2be1f9f6597bdbae806045f22e3196f5e027f
Joshua Dickens <schwa23@schwaBookPro.local>
2009-01-15 21:50:36 -0800

    Support for unicode characters

--- Support/bin/beautifier.js
+++ Support/bin/beautifier.js
@@ -2,7 +2,7 @@
 // Uses jsc, the javascript commandline shell, to execute Beautify on selected text or file
 // js_beautify(js_source_text, indent_size, indent_character, indent_level)
 /*extern js_beautify */
-/*global print, quit, readFile */
+/*global print, quit, readFile, escape */
 
 (function(a) {
   var options = a[0]; //JSON options from commandline
@@ -10,6 +10,8 @@
   //parse options
   //simple test for json hash format
   if (options && options[0] === '{' && options[options.length - 1] === '}') {
+    //turn our options into an object (this is hacky because of the way
+    //jsc works)
     eval("options = " + options);
     // print("options: selection" + options.selection);
   } else {
@@ -26,8 +28,10 @@
     // print("nothing to format");
   }
 
+  // sel = unescape(encodeURIComponent(sel));
   // js_beautify(js_source_text, indent_size, indent_character, indent_level)
   output = js_beautify(sel, options.tabSize, " ");
+  //this takes the output and decodes it to UTF8 (it gets encoded by readFile, apparently)
+  output = decodeURIComponent(escape(output));
   print(output);
-
 })(arguments);
